a3e4fdfbf2e6c1bfa5af2a52d98fc6201e179f72
boraidrizaj <117390325+boraidrizaj@users.noreply.github.com>
2022-11-03 19:34:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+})
